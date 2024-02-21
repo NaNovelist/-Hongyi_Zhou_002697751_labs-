9edc90edebf917547872859c544c885d7254aacb
NaNovelist <145168591+NaNovelist@users.noreply.github.com>
2024-02-20 23:40:46 -0500

    Add files via upload

---
+++ Lab_4/src/model/Supplier.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author hyZHO
+ */
+public class Supplier {
+    private String supplyName;
+    private ProductCatalog productCatalog;
+    private ImageIcon logoImage;
+    
+    public Supplier(){
+        productCatalog = new ProductCatalog();
+    }
+
+    public String getSupplyName() {
+        return supplyName;
+    }
+
+    public void setSupplyName(String supplyName) {
+        this.supplyName = supplyName;
+    }
+
+    public ProductCatalog getProductCatalog() {
+        return productCatalog;
+    }
+
+    public void setProductCatalog(ProductCatalog productCatalog) {
+        this.productCatalog = productCatalog;
+    }
+
+    public ImageIcon getLogoImage() {
+        return logoImage;
+    }
+
+    public void setLogoImage(ImageIcon logoImage) {
+        this.logoImage = logoImage;
+    }
+    
+    @Override
+    public String toString(){
+        return supplyName;
+    }
+}
